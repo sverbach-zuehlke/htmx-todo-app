4ce2e5248b77c6d7c8bd1f38b0597af2c701d84c
Sven Erbach <sven.erbach@zuehlke.com>
2023-11-15 16:18:17 +0100

    wip

--- server.ts
+++ server.ts
@@ -9,19 +9,25 @@ const PORT = process.env.PORT || 3001;
 type Todo = {
     id: string,
     name: string,
-    done: boolean
+    description: string,
+    done: boolean,
+    createdAt: Date
 }
 
 let todos: Todo[] = [
     {
-        id: "123",
-        name: 'Taste htmx',
-        done: true
+        id: uuid(),
+        name: 'Uninstall htmx',
+        description: 'After trying out htmx I want to commit uninstall.',
+        done: true,
+        createdAt: new Date(new Date().getTime() - 24 * 3600 * 1000)
     },
     {
         id: uuid(),
-        name: 'Buy a unicorn',
-        done: false
+        name: 'setup neovim for 3 hrs',
+        description: 'goal is to have neovim configured for my dotnet project',
+        done: false,
+        createdAt: new Date()
     }
 ];
 
@@ -40,9 +46,23 @@ app.get('/', (req, res ) => {
 app.delete('/todos/:id', (req,res) => {
     const { id } = req.params;
     todos = todos.filter(t => t.id !== id);
-    res.sendStatus(200);
+    res.sendStatus(200); // never, under ZERO circumstances, send 204, I dare you!
 });
 
+app.post('/todos', (req, res) => {
+    const {name} = req.body;
+    const todo: Todo = {id: uuid(), name, description: '', done: false, createdAt: new Date()};
+    todos.push(todo);
+    const template = pug.compileFile('views/components/todo-item.pug');
+    const markup = template({ todo });
+    res.status(201).send(markup);
+});
+
+app.get('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    const todo = todos.find(t => t.id === id);
+
+})
 
 app.listen(PORT);
 
